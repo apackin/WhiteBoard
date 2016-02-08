68e63568967c37f96e29ce36859e9a93069f6b07
apackin <apackin@gmail.com>
2016-02-08 10:22:15 -0500

    added persistance

--- browser/app.js
+++ browser/app.js
@@ -1,12 +1,7 @@
 var socket = io(window.location.origin);
 
 socket.on('connect', function (storedArt) {
-
    console.log('I have made a persistent two-way connection to the server!');
-
-    // storedArt.forEach(function(arr){
-    // 	whiteboard.draw(arr[0], arr[1], arr[2]);
-    // });
 });
 
 whiteboard.on("draw", function(start, end, strokeColor) {
@@ -16,5 +11,11 @@ whiteboard.on("draw", function(start, end, strokeColor) {
 
 socket.on('someoneDrew', function(start, end, strokeColor) {
     whiteboard.draw(start, end, strokeColor);
-    // console.log(start, end, strokeColor);
 });
+
+socket.on('persist', function(storedArt){
+	storedArt.forEach(function(arr){
+    	whiteboard.draw(arr[0], arr[1], arr[2]);
+    });
+});
+
--- server.js
+++ server.js
@@ -2,35 +2,29 @@ var path = require('path');
 var socketio = require('socket.io');
 var http = require('http');
 var server = http.createServer();
-
 var express = require('express');
 var app = express();
-
 server.on('request', app);
 
 var io = socketio(server);
 
-var storedArt = [];
 
+var storedArt = [];
 
 
 io.on('connection', function (socket) {
     /* This function receives the newly connected socket.
        This function will be called for EACH browser that connects to our server. */
     console.log('A new client has connected!');
-    storedArt.forEach(function(thing){
-    	socket.broadcast.emit(thing);
-    });
-    // for each thing in the array send someoneDrew.
+    socket.emit('persist', storedArt);
 
 	socket.on('disconnect', function() {
 		console.log("We have lost client: ", socket.id);
 	});
 
 	socket.on('drawing', function(start, end, strokeColor){
-		storedArt.push('someoneDrew', start, end, strokeColor);
+		storedArt.push([start, end, strokeColor]);
 		socket.broadcast.emit('someoneDrew', start, end, strokeColor);
-		/// push to the array above
 	});
 
 });
